8d6e9b6490be85b0de58d79f476f0ad989150ab1
Phil Evans <email@philevans.co>
2022-07-11 12:35:38 -0300

    UX/CSS tweaks

--- site/assets/themes/fw-child/previews/overlay-risks-indicator.php
+++ site/assets/themes/fw-child/previews/overlay-risks-indicator.php
@@ -73,5 +73,5 @@
 
 	?>
 >
-	<a href="<?php echo $GLOBALS['vars']['site_url']; ?>risks/#<?php echo get_field ( 'indicator_key', $item['id'] ); ?>"><?php echo $item['title']; ?></a>
+	<a href="<?php echo $GLOBALS['vars']['site_url']; ?>risks/#<?php echo get_field ( 'indicator_key', $item['id'] ); ?>" class="risk-var-link d-block p-2"><?php echo $item['title']; ?></a>
 </div>
