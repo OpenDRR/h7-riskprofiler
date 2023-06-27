adab6a45f98f8f1a5742d82b4b16e0f7249b1317
Anthony Fok <anthony.fok@nrcan-rncan.gc.ca>
2023-06-27 15:26:53 -0600

    Append build number (based on "git describe") to the site version
    See my previous temporary hack from July 2022 at
    OpenDRR/riskprofiler commit f380694c117f2674be8a81d4f24b4b3532bc9db2

--- site/assets/themes/fw-child/template/version.php
+++ site/assets/themes/fw-child/template/version.php
@@ -12,4 +12,21 @@
 
 	echo '<span class="mx-1">•</span>';
 
+	if ($git_describe = get_option ( 'options_git_describe' )) {
+		list($version, $api_version, $release_date, $commits_since, $commit_hash) = explode("-", $git_describe);
+
+		if ($commits_since == 0) {
+			$git_describe = implode('-', [$version, $api_version, $release_date]);
+		}
+
+		$build_number = sprintf("%04d", $release_date - 20220000);
+		if ($commits_since > 0) {
+			$build_number = implode('-', [$build_number, $commits_since, $commit_hash]);
+		}
+
+		echo '(<a href="https://github.com/OpenDRR/riskprofiler/tree/' . $git_describe . '" class="text-gray-400">' . $build_number . '</a>)';
+
+		echo '<span class="mx-1">•</span>';
+	}
+
 ?></p>
